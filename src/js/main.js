ce161847cdbdbc6c0767a22497d74de79fa20046
María Simó <mariasimo@coponstudio.es>
2020-07-20 10:09:39 +0200

    Implement basic tab switch funcionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,12 @@
 // No touch screen detection
-function is_touch_enabled() { 
+function isTouchEnabled() { 
     return ( 'ontouchstart' in window ) ||  
            ( navigator.maxTouchPoints > 0 ) ||  
            ( navigator.msMaxTouchPoints > 0 ); 
 } 
 
-window.addEventListener('load', () => {
-
-    // Add class to body on no-touch-devices
-    const bodyTag = document.querySelector('body')
-    bodyTag.classList.add(!is_touch_enabled() && 'no-touch-device')
-
-    // Add class to open mobile menu
+// Add class to open mobile menu
+function headerMenuToggle() {
     const mobileHeader = document.querySelector('header .tablet-only')
     const openMenuBtn = mobileHeader.querySelector('.megamenu-mobile__trigger.header-btn')
     const toggleMenu = (menu) => {
@@ -20,5 +15,47 @@ window.addEventListener('load', () => {
     }
 
     openMenuBtn.addEventListener('click', () => toggleMenu(mobileHeader))
+}
+
+
+// Add tabs functionality
+function tabsHandler() {
+    const tabItems = document.querySelectorAll('.tab-menu-item')
+    const tabContents = document.querySelectorAll('.tab-content-item')
+
+    tabItems.forEach(tabItem => {
+        tabItem.addEventListener('click', (e) => _switchTabs(e, tabItems, tabContents))
+    })
+}
+
+function _switchTabs(e, tabItems, tabContents) {
+    // Change tab style
+    const activeTab = e.currentTarget
+    tabItems.forEach(tabItem => {
+        tabItem.classList.remove('current')
+    })
+    activeTab.classList.add('current')
+
+    // Show active tab content
+    const activeTabContent = document.getElementById(activeTab.getAttribute('data-tabitem'))
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove('current')
+    })
+    activeTabContent.classList.add('current')
+}
+
+
+window.addEventListener('load', () => {
+
+    // Add class to body on no-touch-devices
+    const bodyTag = document.querySelector('body')
+    bodyTag.classList.add(!isTouchEnabled() && 'no-touch-device')
+
+    // Add class to open mobile menu
+    headerMenuToggle()
+
+    // Add tabs functionality
+    tabsHandler()
     
 })
+
